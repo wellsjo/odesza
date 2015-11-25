d5d05db7c7bc1874fe48eecfeef9a1e8270bfa8d
Joseph Werle <joseph.werle@gmail.com>
2015-11-25 17:41:47 -0500

    Add test and wrap in closure

---
+++ test/index.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const assert = require('assert');
+const odesza = require('../');
+const test = require('tape');
+const path = require('path');
+const fs = require('fs');
+
+const read = file => fs.readFileSync(file, 'utf-8');
+const fixture = file => path.join(__dirname, 'fixtures', file);
+
+test('simple', (t) => {
+  let context = odesza({value: 'world'});
+  let string = context.compile(fixture('simple.txt'));
+  t.ok(string == 'hello world', 'template compiles');
+  t.end();
+});
+
