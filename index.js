6b1923121eaa392ec19d617a2ae2e8905da86fd3
Wells Johnston <wellsjohnston@gmail.com>
2015-11-25 16:13:41 -0500

    fixed bug in express support and added comments

--- index.js
+++ index.js
@@ -1,21 +1,57 @@
+/**
+ * Flexible template engine powered by ES6 template strings.
+ *
+ * @author Wells Johnston <wells@littlstar.com>
+ */
+
 'use strict'
 
 var fs = require('fs');
 var vm = require('vm');
 
 var odesza = {};
+module.exports = odesza;
 
-odesza.render = (path, vars) => {
+/**
+ * Renders a template with the given variables.
+ *
+ * @param {string} template The template to render.
+ * @param {object} vars An object of key-value pairs representing the
+ * variables to be used in the template.
+ * @return {string} The rendered template.
+ */
+
+odesza.render = (template, vars) => {
   try {
-    let t = fs.readFileSync(path).toString();
-    return vm.runInNewContext('`' + t + '`', vars);
+    return vm.runInNewContext('`' + template + '`', vars);
   } catch (e) {
     throw new Error(e);
   }
 };
 
-odesza.__express = (path, options, fn) => {
-  return fn(this.render());
+/**
+ * @param {string} path The path to the template file.
+ * @param {object} options Options passed in to render the template.
+ * @return {string} The rendered template.
+ */
+
+odesza.compile = (path, options) => {
+  try {
+    var template = fs.readFileSync(path).toString();
+  } catch (e) {
+    throw new Error(e);
+  }
+  return this.render(template, options);
 };
 
-module.exports = odesza;
+/**
+ * Adds support for express.
+ *
+ * @param {string} path
+ * @param {object} options
+ * @param {function} fn
+ */
+
+odesza.__express = (path, options, fn) => {
+  return fn(this.compile(path, options));
+};
