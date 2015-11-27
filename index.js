aeb11d9e89274c3d5414514e3f06a857452e4604
Wells Johnston <wellsjohnston@gmail.com>
2015-11-27 10:32:03 -0500

    moved import back to index file, removed middleware for now

--- index.js
+++ index.js
@@ -6,10 +6,8 @@
 
 'use strict'
 
-var extend = require('extend');
 var fs = require('fs');
 var vm = require('vm');
-var __import = require('./lib/import');
 
 /**
  * Creates an odesza object.
@@ -18,109 +16,98 @@ var __import = require('./lib/import');
  * @return {Object}
  */
 
-module.exports = function createOdesza(context) {
+var odesza = {};
+module.exports = odesza;
 
-  // create scope for this instance
-  var scope = extend(true, {}, context || {});
+/**
+ * Renders a template with the given variables.
+ *
+ * @param {string} template The template to render.
+ * @param {object} vars An object of key-value pairs representing the
+ * variables to be used in the template.
+ * @return {string} The rendered template.
+ */
 
-  /**
-   * odesza object.
-   */
+odesza.render = function(template, vars) {
 
-  var odesza = {};
+  vars = vars && 'object' == typeof vars ? vars : {};
 
-  /**
-   * odesza middleware.
-   */
+  template = resolveImports(template);
 
-  var middleware = [];
-
-  /**
-   * Renders a template with the given variables.
-   *
-   * @param {string} template The template to render.
-   * @param {object} vars An object of key-value pairs representing the
-   * variables to be used in the template.
-   * @return {string} The rendered template.
-   */
-
-  odesza.render = function(template, vars) {
-
-    vars = vars && 'object' == typeof vars ? vars : {};
-
-    var renderScope = extend(true, scope, vars);
-    var ctx = {
-      template: template,
-      context: renderScope
-    };
-
-    middleware.forEach(ware => ware.call(odesza, ctx));
-
-    try {
-      return vm.runInNewContext('`' + (ctx.template || template) + '`', renderScope);
-    } catch (e) {
-      throw new Error(e);
-    }
-  };
-
-  /**
-   * Compiles a template file.
-   *
-   * @param {string} path The path to the template file.
-   * @param {object} options Options passed in to render the template.
-   * @return {string} The rendered template.
-   */
-
-  odesza.compile = function(path, options) {
-    try {
-      var template = fs.readFileSync(path).toString().trim();
-    } catch (e) {
-      throw new Error(e);
-    }
-    return odesza.render(template, options);
-  };
-
-  /**
-   * Install plugin.
-   *
-   * @param {Function} fn Middlware
-   * @return {Object} odesza
-   */
-
-  odesza.use = function(fn) {
-    if ('function' == typeof fn) {
-      middleware.push(fn);
-    } else {
-      throw new TypeError('fn is not Function type.')
-    }
-    return this;
-  };
-
-  /**
-   * Adds support for import('') syntax.
-   */
-
-  odesza.use(__import);
-
-  /**
-   * Adds support for express.
-   *
-   * @param {string} path
-   * @param {object} options
-   * @param {function} fn
-   */
-
-  odesza.__express = function(path, options, fn) {
-    try {
-      return fn(null, odesza.compile(path, options));
-    } catch (e) {
-      return fn(e);
-    }
-  };
-
-  /**
-   * odesza instance.
-   */
-
-  return odesza;
+  try {
+    return vm.runInNewContext('`' + template + '`', vars);
+  } catch (e) {
+    throw new Error(e);
+  }
 };
+
+/**
+ * Compiles a template file.
+ *
+ * @param {string} path The path to the template file.
+ * @param {object} options Options passed in to render the template.
+ * @return {string} The rendered template.
+ */
+
+odesza.compile = function(path, options) {
+  try {
+    var template = fs.readFileSync(path).toString().trim();
+  } catch (e) {
+    throw new Error(e);
+  }
+  return odesza.render(template, options);
+};
+
+/**
+ * Install plugin.
+ *
+ * @param {Function} fn Middlware
+ * @return {Object} odesza
+ */
+
+odesza.use = function(fn) {
+  if ('function' == typeof fn) {
+    odesza.middleware.push(fn);
+  } else {
+    throw new TypeError('Middleware must provide a function.')
+  }
+  return this;
+};
+
+/**
+ * Adds support for express.
+ *
+ * @param {string} path
+ * @param {object} options
+ * @param {function} fn
+ */
+
+odesza.__express = function(path, options, fn) {
+  try {
+    return fn(null, odesza.compile(path, options));
+  } catch (e) {
+    return fn(e);
+  }
+};
+
+/**
+ * Adds support for import keyword.
+ */
+
+function resolveImports(template) {
+
+  // matches import('file'), import("file")
+  var rgx = /(import|require)\([\'\"]([\w.]+)[\'\"]\)/g;
+
+  // make matches values unique
+  var matches = template.match(rgx) || [];
+  matches = matches.filter((m, i) => matches.indexOf(m) == i);
+
+  // replace each import match with the compiled version
+  matches.forEach(match => {
+    let name = match.split('\'')[1];
+    template = template.split(match).join(odesza.compile(name));
+  });
+
+  return template;
+}
