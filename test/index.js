f9d961af2a9ea66d5dd568468496934cb0103a4c
Wells Johnston <wellsjohnston@gmail.com>
2015-11-30 11:49:24 -0500

    fixed test dependencies

--- test/index.js
+++ test/index.js
@@ -1,10 +1,8 @@
 'use strict';
 
-const assert = require('assert');
 const odesza = require('../');
 const test = require('tape');
 const path = require('path');
-const diff = require('diff');
 const fs = require('fs');
 const fixture = file => path.join(__dirname, 'fixtures', file);
 
