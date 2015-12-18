a4474b1ad27f22e80b15e98b3c3d5f396059da89
Wells Johnston <wellsjohnston@gmail.com>
2015-12-18 16:01:16 -0500

    variables for odesza templates now work from the command line

--- cli.js
+++ cli.js
@@ -1,19 +1,32 @@
 #!/usr/bin/env node
 
-var program = require('commander');
-var fs = require('fs');
-var package = require('./package.json');
-var odesza = require('./');
+'use strict'
+
+const fs = require('fs');
+const program = require('commander');
+const odesza = require('./');
+const app = require('./package.json');
 
 program
-  .version(package.version)
-  .usage('<file> [options]')
+  .version(app.version)
+  .usage('<file> [options] var1=val,var2=val2]')
   .description('Compiles odesza templates.')
   .option('-o, --output', 'output file')
   .parse(process.argv);
 
+const file = program.args.shift();
+if (program.output) {
+  const output = program.args.shift();
+}
 
-var template = odesza.compile(program.args[0]);
+const vars = {};
+
+program.args.forEach(arg => {
+  let parts = arg.split('=');
+  vars[parts[0]] = parts[1];
+});
+
+const template = odesza.renderFile(file, vars);
 
 if (program.output) {
   fs.writeFileSync(program.args[1], template);
